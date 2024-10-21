993560ddad0932908daade0c362ae369650d2b4c
jamesk02 <jvhkirkham@gmail.com>
2024-10-21 15:37:46 +0100

    add _dirname

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
